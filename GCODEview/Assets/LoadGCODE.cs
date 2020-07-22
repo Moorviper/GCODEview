960a34598f5ec068d4c42673d80297503e1bf278
Daniel Krah <moorviper@gmail.com>
2020-07-22 17:08:25 +0200

    [update] LoadGCODE.cs
    implemented basic satinizer

--- GCODEview/Assets/LoadGCODE.cs
+++ GCODEview/Assets/LoadGCODE.cs
@@ -17,23 +17,23 @@ public class LoadGCODE : MonoBehaviour
     DirectoryInfo directoryInfo = null;
     FileInfo[] fileInfo = null;
     string[] actualSelectedGcode = null;
-    int lastSelection = -1;
+    int lastSelection = 0;
     // Start is called before the first frame update
-    string[][] parsedGcode = null;
+    string[][] parsedGcode;
     void Start()
     {
+        parsedGcode[0] = new string[] { "Test", "sdads", "dfdf" };
+        // parsedGcode[0] = testa;
         // ReadGcodeDir();
     }
-
     void Update()
     {
+        // Debug.Log("read gcode dir :   Last selection: " + lastSelection + "  value selected : " + GameObject.Find("loadGcode").GetComponent<Dropdown>().value);
         ReadGcodeDir();
         if (lastSelection != GameObject.Find("loadGcode").GetComponent<Dropdown>().value)
         {
             LoadGcodeFile();
         }
-        
-
     }
 
     void ReadGcodeDir()
@@ -47,6 +47,8 @@ public class LoadGCODE : MonoBehaviour
         directoryInfo = new DirectoryInfo(gcodeDir);
         FileInfo[] fileInfo = directoryInfo.GetFiles("*.*", SearchOption.AllDirectories);
 
+        Debug.Log("[update] file tree");
+
         foreach (FileInfo file in fileInfo)
         {
             if (file.Name == ".DS_Store") { } // do not load Spotlight files
@@ -59,35 +61,36 @@ public class LoadGCODE : MonoBehaviour
         }
         // lastSelection = 0;
     }
-
-
     void LoadGcodeFile()
     {
         // Log gcode file to Log
         // for testing set selected file to file 1 in the list
         // GameObject.Find("loadGcode").GetComponent<Dropdown>().value = 1;
+        // arr = new int[2][];
         if (GameObject.Find("loadGcode").GetComponent<Dropdown>().value != 0)
         {
             Dropdown test = GameObject.Find("loadGcode").GetComponent<Dropdown>();
             string newpath = gcodeDir + "/" + test.options[test.value].text;
             actualSelectedGcode = File.ReadAllLines(newpath);
-            
+
+            parsedGcode = null;
+            Array.Resize(ref parsedGcode, actualSelectedGcode.Length);
+            // Debug.Log("GCODE Lines: " + actualSelectedGcode.Length);
+            Debug.Log("[loading] Gcode File");
+
             // if (lastSelection != GameObject.Find("loadGcode").GetComponent<Dropdown>().value)
             // {
-                ParseGcode();
+            ParseGcode();
             // }
             lastSelection = GameObject.Find("loadGcode").GetComponent<Dropdown>().value;
         }
-
-
-
-
     }
 
     void ParseGcode()
     {
         for (int i = 0; i < actualSelectedGcode.Length; i++)
         {
+            parsedGcode[i] = new string[10];
             if (actualSelectedGcode[i].StartsWith(";"))
             {
                 if (actualSelectedGcode[i].StartsWith(";Dimension"))
@@ -114,79 +117,89 @@ public class LoadGCODE : MonoBehaviour
                 {
                     // Debug.Log("code length " + actualSelectedGcode[i].Length);
                     string[] g0 = actualSelectedGcode[i].Split(' ');
-                    Debug.Log("G0 code length " + g0.Length);
+                    // Debug.Log("debug go : " + g0);
+                    // Debug.Log("G0 code length " + g0.Length);
+                    String x_temp;
+                    String y_temp;
+                    String z_temp;
+                    String f_temp;
+                    String e_temp;
 
                     for (int l = 0; l < g0.Length; l++)
                     {
                         switch (g0[l][0])
                         {
                             case 'F':
-                                Debug.Log("F");
-                                parsedGcode[i][0] = g0[l];
+                                f_temp = g0[l];
                                 break;
                             case 'X':
-                                Debug.Log("X");
-                                parsedGcode[i][1] = g0[l];
+                                x_temp = g0[l];
                                 break;
                             case 'Y':
-                                Debug.Log("Y");
-                                parsedGcode[i][2] = g0[l];
+                                y_temp = g0[l];
                                 break;
                             case 'Z':
-                                Debug.Log("Z");
-                                parsedGcode[i][3] = g0[l];
+                                y_temp = g0[l];
                                 break;
-
+                            case 'E':
+                                e_temp = g0[l];
+                                break;
                             default:
-                            break;
+                                break;
                         }
+                        parsedGcode[i] = new String[] {"G0", x_temp, y_temp, z_temp, f_temp, e_temp };
                     }
-
-
-
-
-
-
-
-                    // bedX = float.Parse(g0[1]);
-                    // bedY = float.Parse(g0[2]);
-                    // bedZ = float.Parse(g0[3]);
-                    // nozzleDiameter = float.Parse(g0[4]);
-
-                    // Debug.Log(bedX + " " + bedY + " " + bedZ + " " + nozzleDiameter);
-
-
-
-                    // SetBuildplate(bedX, bedY);
-                    // Debug.Log(actualSelectedGcode[i]);
                 }
-                // if (actualSelectedGcode[i].StartsWith("G1"))
-                // {
-                //     string[] g1 = actualSelectedGcode[i].Split(' ');
-                //     bedX = float.Parse(g1[1]);
-                //     bedY = float.Parse(g1[2]);
-                //     bedZ = float.Parse(g1[3]);
-                //     nozzleDiameter = float.Parse(g1[4]);
+                if (actualSelectedGcode[i].StartsWith("G1"))
+                {
+                    // Debug.Log("code length " + actualSelectedGcode[i].Length);
+                    string[] g1 = actualSelectedGcode[i].Split(' ');
+                    // Debug.Log("debug go : " + g0);
+                    // Debug.Log("G0 code length " + g0.Length);
+                    String x_temp;
+                    String y_temp;
+                    String z_temp;
+                    String f_temp;
+                    String e_temp;
 
-                //     Debug.Log(bedX + " " + bedY + " " + bedZ + " " + nozzleDiameter);
+                    for (int l = 0; l < g0.Length; l++)
+                    {
+                        switch (g1[l][0])
+                        {
+                            case 'F':
+                                f_temp = g1[l];
+                                break;
+                            case 'X':
+                                x_temp = g1[l];
+                                break;
+                            case 'Y':
+                                y_temp = g1[l];
+                                break;
+                            case 'Z':
+                                y_temp = g1[l];
+                                break;
+                            case 'E':
+                                e_temp = g0[l];
+                                break;
+                            default:
+                                break;
+                        }
+                        parsedGcode[i] = new String[] {"G1", x_temp, y_temp, z_temp, f_temp, e_temp };
+                    }
+                }
+            }
+        }
 
+        void SetBuildplate(float x, float y)
+        {
+            if (GameObject.Find("loadGcode").GetComponent<Dropdown>().value != 0)
+            {
+                GameObject.Find("buildplate").transform.position = new Vector3(x / 2 / 1000, -1, y / 2 / 1000);
+                GameObject.Find("buildplate").transform.localScale = new Vector3(x / 1000, 1, y / 1000);
+                GameObject.Find("Main Camera").transform.position = new Vector3(x / 2 / 1000, 200, -200);
 
-
-                //     SetBuildplate(bedX, bedY);
-                //     Debug.Log(actualSelectedGcode[i]);
-                // }
+                // Transform bp = GameObject.Find("buildplate").transform;
+                // // GameObject.Find("MainCamera").transform.LookAt(bp);
             }
         }
     }
-
-    void SetBuildplate(float x, float y)
-    {
-        if (GameObject.Find("loadGcode").GetComponent<Dropdown>().value != 0)
-        {
-            GameObject.Find("buildplate").transform.position = new Vector3(x / 2 / 1000, -1, y / 2 / 1000);
-            GameObject.Find("buildplate").transform.localScale = new Vector3(x / 1000, 1, y / 1000);
-            // Transform bp = GameObject.Find("buildplate").transform;
-            // // GameObject.Find("MainCamera").transform.LookAt(bp);
-        }
-    }
-}
